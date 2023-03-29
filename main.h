d42a6fa10d622250e02ad6fdc8a788e1c186ee58
mwath1 <mwathiann201@yahoo.com>
2023-03-29 06:18:14 -0700

    updated main.h

--- main.h
+++ main.h
@@ -17,9 +17,11 @@
 #define S _LONG 2
 
 
-/*
- * fmt: stands for the format in our case
- * fm_t : the funcction we will use
+/**
+ * struct fmt - Struct op
+ * @fmt: stands for the format in our case
+ * @fn: function associated
+ * @fm_t : the funcction we will use
  */
 struct fmt 
 {
@@ -27,10 +29,33 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 
 };
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 
-int handle_print(const char *fmt, int *i);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/******* FUNCTIONS *******/
+
+/* Funtions to print, strings, percent, integer and binary */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
 
 #endif
