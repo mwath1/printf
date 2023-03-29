7cd72a8a2897bd80c760d32dfbd22c28b0376797
mwath1 <mwathiann201@yahoo.com>
2023-03-29 09:31:19 -0700

    updated main.h

--- main.h
+++ main.h
@@ -3,18 +3,19 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-#define F _MINUS 1
-#define F _PLUS 2
-#define F _ZERO 4
-#define F _HASH 8
-#define F _SPACE 16
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
 
-#define S _SHORT 1
-#define S _LONG 2
+#define S_SHORT 1
+#define S_LONG 2
 
 
 /**
