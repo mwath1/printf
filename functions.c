a83523046017f30b472fed9f1faa460d77e10805
Nellyssc <nellysalama0@gmail.com>
2023-03-30 02:40:25 -0700

    K

--- functions.c
+++ functions.c
@@ -1,5 +1,4 @@
 #include "main.h"
-/******* PRINT CHAR *******/
 /**
  * print_char - Prints char
  * @types: List arguments
@@ -18,8 +17,6 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-
-/******* PRINT STRING *******/
 
 /**
  * print_string - Prints a string
@@ -76,9 +73,6 @@ int print_string(va_list types, char buffer[],
 	}
 	return (write(1, str, length));
 }
-
-/******* PRINT PERCENT SIGN *******/
-
 /**
  * print_percent - Prints a percent sign
  * @types: List arguments
@@ -101,98 +95,3 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(size);
 	return (write(1, "%%", 1));
 }
-
-/******* PRINT INT *******/
-
-/**
- * print_int - Prints integer
- * @types: List arguments
- * @buffer: holds array to print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: No. of chars printed
- */
-
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	int i = BUFF_SIZE - 2;
-	int is_negative = 0;
-	long int n = va_arg(types, long int);
-	unsigned long int num;
-
-	n = convert_size_number(n, size);
-
-	if (n == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-	num = (unsigned long int)n;
-
-	if (n < 0)
-	{
-		num = (unsigned long int)((-1) * n);
-		is_negative = 1;
-	}
-
-	while (num > 0)
-	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
-	}
-
-	i++;
-
-	return (write_number(is_negative, i, buffer, flags, width, precision, size));
-}
-
-/******* PRINT BINARY *******/
-
-/**
- * print_binary - Prints an unsigned number
- * @types: List arguments
- * @buffer: holds array to print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: No. of chars printed
- */
-
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
-
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-
-	n = va_arg(types, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
-	{
-		m /= 2;
-		a[i] = (n / m) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
-		{
-			char z = '0' + a[i];
-
-			write(1, &z, 1);
-			count++;
-		}
-	}
-	return (count);
-}
-
